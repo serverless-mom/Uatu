4fcb7e021c598e83f198217fae1a4780ba1ff38d
System Administrator <root@Tobys-MacBook-Pro.local>
2015-06-30 16:52:55 -0700

    non-working as I try to create objects

--- statgetter.js
+++ statgetter.js
@@ -1,39 +1,55 @@
-var os=require('os');
-var CronJob = require('cron').CronJob;
-var ps = require('ps-node');
+var os=require('os')
+var CronJob = require('cron').CronJob
+var exec = require('exec')
+//type-of-is is solely for debugging
+var Type = require('type-of-is')
+var util = require('util')
 
-//these two requirements are so I can run arbitrary execs, may not go to prod
-var sys = require('sys')
-var exec = require('child_process').exec;
+var fifteenMinNormalizedLoadAvg = 0.0
+var currentData = {};
+
+//prints the current date so I can make sure the cron is running like it should
+
+function printExec(cmd){
+  child = exec(cmd
+    //TODO test on QA and prod how frequently we have to kill this child.
+    , {timeout: 4500} //will kill this child process if it takes more than 4500ms to come back
+    , function (error, stdout, stderr) {
+
+      //this is fun but you can't redefine properties :()
+      //TODO: make the commands an array that we iterate through and define properties.
+      //Object.defineProperty(currentData, cmd, {value: stdout})
 
 
-var fifteenMinNormalizedLoadAvg = 0.0;
+      console.log('stdout with command '+cmd+': ' + stdout)
+      //okay not certain why, but stderr is coming out as '0' and error non-null
+      //TODO: make that stop happening, maybe a result of OSX machine...
+      //console.log('stderr: ' + stderr)
+      //if (error !== null) {
+      //  console.log('exec error: ' + error);
+      //}
+       console.log(util.inspect(currentData))
+  });
+}
 
-var ps = require('ps-node');
+function printCPU(){
+    fifteenMinNormalizedLoadAvg = os.loadavg()[2]/os.cpus().length;
+    console.log("fifteen minute load avg, normalized by CPU number: "+
+    fifteenMinNormalizedLoadAvg)
+}
 
-// A simple pid lookup
-ps.lookup({
-    command: 'node',
-    }, function(err, resultList ) {
-    if (err) {
-        throw new Error( err );
-    }
-    console.log("I found "+resultList.length+" results")
-
-    resultList.forEach(function( process ){
-        if( process ){
-
-            console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments );
-        }
-    });
-});
-
+function printFreeMemPercent(){
+  console.log ("Free mem percentage: "+os.freemem()/os.totalmem() * 100)
+}
+//this takes cron notation which I guess always allowed seconds? ikr? weird?
 new CronJob('* * * * * *', function(){
-  //prints the current date so I can make sure the cron is running like it should
-  exec("date", console.log);
+  printExec("date")
+  printCPU()
+  printFreeMemPercent()
+  printExec("ps")
+  printExec("df -h")
+  printExec("top -l 1 | grep 'Chrome'|tail -n 1")
 
-  fifteenMinNormalizedLoadAvg = os.loadavg()[2]/os.cpus().length;
 
-  console.log("fifteen minute load avg, normalized by CPU number: "+
-  fifteenMinNormalizedLoadAvg);
-}, null, true, "America/Los_Angeles");
+
+}, null, true, "America/Los_Angeles")
